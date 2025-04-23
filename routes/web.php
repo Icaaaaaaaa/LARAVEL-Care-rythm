d1a6b6f953327ec12e741bcb78fadcee39174223
Icaaaaaaaa <disaajah37@gmail.com>
2025-04-23 22:40:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+//kegiatan
 Route::resource('/kegiatan', KegiatanController::class);
-=======
+
 // Pencapaian
 use App\Http\Controllers\PencapaianController;
 
@@ -20,4 +20,3 @@ Route::post('/pencapaian/reset', [PencapaianController::class, 'reset']);
 Route::post('/pencapaian/tambah-kegiatan', [PencapaianController::class, 'tambahKegiatan']);
 Route::post('/pencapaian/hapus', [PencapaianController::class, 'hapus']);
 
->>>>>>> a520490ad4caecbe3ac7aaa7e6b19b3b0d8f3765
